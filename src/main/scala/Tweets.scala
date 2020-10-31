74f88d4ba1c0aa0af3e5309d95fd486ba1194559
Hansaj Patel <hansajsp29@gmail.com>
2020-10-30 23:53:03 -0500

    load and display data

--- src/main/scala/Tweets.scala
+++ src/main/scala/Tweets.scala
@@ -1,6 +1,23 @@
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
 object Tweets {
   def main(args: Array[String]): Unit = {
     println("Hello World!")
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[1]")
+      .appName("SparkByExample")
+      .getOrCreate()
+
+    val inputFilePath = "Tweets.csv"
+    val tweetsData = spark.read.format("csv").option("header", "true").load(inputFilePath)
+
+    tweetsData.show(20)
+
+
   }
 
 }
